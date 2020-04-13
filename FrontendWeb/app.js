ddcdb02b9dc5a3b82135fc7e4c63aef16841ebb6
Sedai <quentin.beaupuy@gmail.com>
2020-04-13 14:50:01 +0200

    init

---
+++ FrontendWeb/app.js
@@ -0,0 +1,15 @@
+$(() => {
+  // let jsmediatags = window.jsmediatags;
+  //   console.log("aaa");
+  // jsmediatags.read("https://drive.google.com/uc?id=1QzqtWXki3yJdS2n6z8kLwvHDZYPogZTL", {
+  //   onSuccess: function(tag) {
+  //     console.log("success");
+  //     console.log(tag);
+  //   },
+  //   onError: function(error) {
+  //     console.log("error");
+  //     console.log(error);
+  //   }
+  // });
+
+});
