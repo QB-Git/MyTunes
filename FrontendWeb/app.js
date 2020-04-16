539808c56b43ad056e596fef3f426580d727b43e
Sedai <quentin.beaupuy@gmail.com>
2020-04-16 20:56:41 +0200

    - Création affichage de base - Création logo (SVG) - Multiples tests (include html, amplitudejs, ...) - Ajout scrollbar personnalisé - Ajout bootstrap - Ajout mdbootstrap

--- FrontendWeb/app.js
+++ FrontendWeb/app.js
@@ -1,28 +1,45 @@
+function getPage(page, nbFois = 1, idBalise = "#contentPage") {
+  if(nbFois > 1) {
+    for (var i = 0; i < nbFois; i++) {
+      $(idBalise).append($('<div>').load(page));
+    }
+  }
+  else $(idBalise).load(page);
+}
+
 $(() => {
-  Amplitude.init({
-    "songs": [
-      {
-        "name": "Song Name 1",
-        "artist": "Artist Name",
-        "album": "Album Name",
-        "url": "audio.mp3",
-        "cover_art_url": "./ressources/music_girl.png"
-      },
-      {
-        "name": "Song Name 2",
-        "artist": "Artist Name",
-        "album": "Album Name",
-        "url": "audio.mp3",
-        "cover_art_url": "./ressources/music_girl.png"
-      },
-      {
-        "name": "Song Name 3",
-        "artist": "Artist Name",
-        "album": "Album Name",
-        "url": "audio.mp3",
-        "cover_art_url": "./ressources/music_girl.png"
-      }
-    ]
-  });
+  //getPage("b.html");
+
+  // $('#sidebarCollapse').on('click', function () {
+  //     $('#sidebar').toggleClass('active');
+  // });
+
+  $('body').overlayScrollbars({ });
+  $('.aaa').overlayScrollbars({ });
+  // Amplitude.init({
+  //   "songs": [
+  //     {
+  //       "name": "Song Name 1",
+  //       "artist": "Artist Name",
+  //       "album": "Album Name",
+  //       "url": "audio.mp3",
+  //       "cover_art_url": "./ressources/music_girl.png"
+  //     },
+  //     {
+  //       "name": "Song Name 2",
+  //       "artist": "Artist Name",
+  //       "album": "Album Name",
+  //       "url": "audio.mp3",
+  //       "cover_art_url": "./ressources/music_girl.png"
+  //     },
+  //     {
+  //       "name": "Song Name 3",
+  //       "artist": "Artist Name",
+  //       "album": "Album Name",
+  //       "url": "audio.mp3",
+  //       "cover_art_url": "./ressources/music_girl.png"
+  //     }
+  //   ]
+  // });
 
 });
