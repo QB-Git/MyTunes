eb48938db8dcf1de1031de86aad799b7c94c1720
nolin1u <33899710+nolin1u@users.noreply.github.com>
2020-05-04 11:30:07 +0200

    Version PLAY PAUSE
    fragment ok
    play pause
    en cours de reliance avec l'API

--- FrontendAndroid/MyTunes/app/src/main/java/com/nolin/mytunes/MainActivity.java
+++ FrontendAndroid/MyTunes/app/src/main/java/com/nolin/mytunes/MainActivity.java
@@ -4,21 +4,88 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
+import java.io.BufferedInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
 public class MainActivity extends AppCompatActivity {
+
+    private MediaPlayer mediaPlayer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //Log.i("Coucou", "erreur au lancement");
+        // Bouton lecture
+        mediaPlayer = MediaPlayer.create(this, R.raw.sound);
+        Button playButton = (Button) findViewById(R.id.start);
+        playButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mediaPlayer.start();
+                mediaPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+                    @Override
+                    public void onCompletion(MediaPlayer mp) {
+                        Toast.makeText(MainActivity.this, "La chanson est finie", Toast.LENGTH_SHORT).show();
+                    }
+                });
+            }
+        });
+        // Bouton pause
+        Button pauseButton = (Button) findViewById(R.id.stop);
+        pauseButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mediaPlayer.pause();
+            }
+        });
+
+        // teste connection
+        URL url;
+        HttpURLConnection urlConnection = null;
+
+        Log.i("Coucou", "c'est avant");
+        try {
+            url = new URL("http://www.android.com/");
+
+            urlConnection = (HttpURLConnection) url
+                    .openConnection();
+
+            InputStream in = urlConnection.getInputStream();
+
+            InputStreamReader isw = new InputStreamReader(in);
+
+            int data = isw.read();
+            while (data != -1) {
+                char current = (char) data;
+                data = isw.read();
+                Log.i("MainActivity", "cuurent ="+current);;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if (urlConnection != null) {
+                urlConnection.disconnect();
+            }
+        }
+
+
+        Log.i("Coucou", "c'est passé");
 
         BottomNavigationView bottomNav = findViewById(R.id.nav_view);
         bottomNav.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -43,7 +110,7 @@ public class MainActivity extends AppCompatActivity {
                 return true;
             }
         });
-
-
     }
+
+
 }
